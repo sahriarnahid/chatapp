3494ab6b7ec99070640e660ee0bc6da69a31d628
Nadeen Menacy <nadinemenessy22@gmail.com>
2025-10-09 20:08:47 +0200

    Changed vite.config.js base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chatapp/', // <--- required for GitHub Pages
+  base: '/', 
   plugins: [react()],
 })
 
