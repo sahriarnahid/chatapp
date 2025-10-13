6dee356fad4b8f1bedf723e79048a234943bd87b
Nadeen Menacy <nadinemenessy22@gmail.com>
2025-10-13 19:55:13 +0200

    Update frontend to use correct Render backend URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://chatapp-xde5.onrender.com/api", // Render backend URL
+      : "https://soder-9mhj.onrender.com/api", // Render backend URL
   withCredentials: true,
 });
