cdc3eb0d9c8b71b468c8ed1fe33096dd91268c33
Nadeen Menacy <nadinemenessy22@gmail.com>
2025-10-15 14:46:01 +0200

    url change Cors again

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.NODE_ENV === "production"
-      ? "https://chatapp-1wf7-git-main-nadeen-menacys-projects.vercel.app/"
+      ? "https://chatapp-1wf7-git-main-nadeen-menacys-projects.vercel.app"
       : "http://localhost:5173",
     credentials: true,
   },
